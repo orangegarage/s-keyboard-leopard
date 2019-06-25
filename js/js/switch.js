2a3a240a96fba91061c4bd031818e85694c524d0
Jin <jjjinyong@naver.com>
2019-06-25 11:35:49 -0400

    added toggle function from changes
    wEEE

---
+++ js/js/switch.js
@@ -0,0 +1,19 @@
+var boxAll= document.getElementsByTagName("input");
+var box1031= document.getElementById("1031check");
+var box0037= document.getElementById("0037check");
+
+function switchFunction(){
+    console.log("function called");
+    if(box1031.checked==true){
+        xkcd1031();
+    }
+    else{
+        uncheck1031();
+    }
+    if(box0037.checked==true){
+        xkcd0037();
+    }
+    else{
+        uncheck0037();
+    }
+}
