4768398931faab0af187f9d24e751f711243c64f
Jin <jjjinyong@naver.com>
2019-06-27 13:28:38 -0400

    Update switch.js

--- js/switch.js
+++ js/switch.js
@@ -1,7 +1,7 @@
 var boxAll= document.getElementsByTagName("input");
 var box1031= document.getElementById("1031check");
 var box0037= document.getElementById("0037check");
-
+// chrome.browserAction.setPopup({});
 function switchFunction(){
     console.log("function called");
     if(box1031.checked==true){
